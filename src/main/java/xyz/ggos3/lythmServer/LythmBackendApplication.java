f66173ea680d94d80912b1d637bec920390ef091
GGos3 <janpll0421@gmail.com>
2023-04-20 22:18:07 +0900

    INIT: SpringInit

---
+++ src/main/java/xyz/ggos3/lythmServer/LythmBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.ggos3.lythmServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LythmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LythmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ggos3/lythmbackend/LythmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ggos3.lythmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LythmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
