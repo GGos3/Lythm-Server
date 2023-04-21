d518da9964c582365e943da48ab73e6b589c26ef
GGos3 <janpll0421@gmail.com>
2023-04-21 11:22:36 +0900

    FEAT: Socket Config 추가

---
+++ src/main/java/xyz/ggos3/lythmServer/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package xyz.ggos3.lythmServer.config;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class WebSocketConfig {
+    @Value("${socket-server.host}")
+    private String host;
+
+    @Value("${socket-server.port}")
+    private int port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        config.setPingTimeout(5000); // 5초
+        return new SocketIOServer(config);
+    }
+}
+
