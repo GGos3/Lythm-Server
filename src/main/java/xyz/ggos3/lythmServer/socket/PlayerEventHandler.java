67963a166c45aedbc6370ef34c1fc43e93378284
GGos3 <janpll0421@gmail.com>
2023-04-22 23:47:17 +0900

    CHORE: 주석처리

--- src/main/java/xyz/ggos3/lythmServer/socket/PlayerEventHandler.java
+++ src/main/java/xyz/ggos3/lythmServer/socket/PlayerEventHandler.java
@@ -1,18 +1,18 @@
-package xyz.ggos3.lythmServer.socket;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import xyz.ggos3.lythmServer.domain.RoomInfo;
-
-import java.util.Map;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class PlayerEventHandler {
-    private final RoomEventHandler roomEventHandler;
-    private final Map<String, RoomInfo> createdRooms = roomEventHandler.getCreatedRooms();
-
-
-}
+//package xyz.ggos3.lythmServer.socket;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import xyz.ggos3.lythmServer.domain.RoomInfo;
+//
+//import java.util.Map;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class PlayerEventHandler {
+//    private final RoomEventHandler roomEventHandler;
+//    private final Map<String, RoomInfo> createdRooms = roomEventHandler.getCreatedRooms();
+//
+//
+//}
