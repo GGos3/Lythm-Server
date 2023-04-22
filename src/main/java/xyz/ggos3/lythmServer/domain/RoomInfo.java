ab0bff816d889964d3b00ed00bdf30b78755e998
GGos3 <janpll0421@gmail.com>
2023-04-22 18:15:30 +0900

    CHORE: RoomInfo 객체 생성자 변경
    기존의 setter 방식에서 생성자를 추가하는 방식으로 변경

--- src/main/java/xyz/ggos3/lythmServer/domain/RoomInfo.java
+++ src/main/java/xyz/ggos3/lythmServer/domain/RoomInfo.java
@@ -14,12 +14,13 @@ public class RoomInfo {
     List<Player> players;
     String roomCode;
 
-    public RoomInfo(String name, String levelCode, String ownerSocketId, int maxPlayer, String roomCode) {
-        this.name = name;
+    public RoomInfo(String levelCode, String ownerSocketId, int curPlayer, int maxPlayer, String roomCode, List<Player> players) {
         this.levelCode = levelCode;
         this.ownerSocketId = ownerSocketId;
+        this.curPlayer = curPlayer;
         this.maxPlayer = maxPlayer;
         this.roomCode = roomCode;
+        this.players = players;
     }
 }
 
