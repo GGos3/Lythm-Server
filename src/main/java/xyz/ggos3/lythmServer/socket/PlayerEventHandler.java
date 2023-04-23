a7af17ba7055f71cb53a01a3bb6e832ce6d5ddd3
GGos3 <janpll0421@gmail.com>
2023-04-23 21:03:17 +0900

    FEAT: roomPlayerReady 이벤트 핸들링

--- src/main/java/xyz/ggos3/lythmServer/socket/PlayerEventHandler.java
+++ src/main/java/xyz/ggos3/lythmServer/socket/PlayerEventHandler.java
@@ -4,11 +4,14 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.annotation.OnEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import xyz.ggos3.lythmServer.domain.Player;
 import xyz.ggos3.lythmServer.domain.RoomInfo;
 import xyz.ggos3.lythmServer.service.RoomEventHandlerService;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 @Slf4j
 @Component
@@ -35,6 +38,32 @@ public class PlayerEventHandler {
         RoomInfo roomInfo = createdRooms.get(code);
         roomInfo.setLevelCode(levelCode);
 
+        service.roomInfoUpdate(client, code, roomInfo);
+    }
+
+    @OnEvent("roomPlayerReady")
+    public void onRoomPlayerReady(SocketIOClient client, String code) {
+        UUID sessionId = client.getSessionId();
+
+        if (code == null){
+            log.info("Error: [roomPlayerReady] cannot Player Ready {} -> {}", sessionId, code);
+            return;
+        }
+
+        log.info("Working: [roomPlayerReady] cannot Player Ready {} -> {}", sessionId, code);
+        updatePlayerState(client, code, "Ready");
+    }
+
+    private void updatePlayerState(SocketIOClient client, String code, String state) {
+        RoomInfo roomInfo = createdRooms.get(code);
+        int index = IntStream.range(0, roomInfo.getPlayers().size())
+                .filter(i -> roomInfo.getPlayers().get(i).getSocketId().equals(client.getSessionId()))
+                .findFirst()
+                .orElse(-1);
+
+        roomInfo.getPlayers().get(index).setState(state);
+
+        createdRooms.put(code, roomInfo);
         service.roomInfoUpdate(client, code, roomInfo);
     }
 }
