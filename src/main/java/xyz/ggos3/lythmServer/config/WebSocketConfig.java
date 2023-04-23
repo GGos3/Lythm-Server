2a78acac3a213d43b37b813015c196d734462bde
GGos3 <janpll0421@gmail.com>
2023-04-23 20:27:53 +0900

    CHORE: import 최적화

--- src/main/java/xyz/ggos3/lythmServer/config/WebSocketConfig.java
+++ src/main/java/xyz/ggos3/lythmServer/config/WebSocketConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import xyz.ggos3.lythmServer.socket.RoomEventHandler;
 
 
 @Configuration
