b74429798bcf7c7fac2d769ec2db6789db6b1a1a
GGos3 <janpll0421@gmail.com>
2023-04-23 20:28:19 +0900

    FEAT: Listener 추가

--- src/main/java/xyz/ggos3/lythmServer/config/Runner.java
+++ src/main/java/xyz/ggos3/lythmServer/config/Runner.java
@@ -4,6 +4,7 @@ import com.corundumstudio.socketio.SocketIOServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import xyz.ggos3.lythmServer.socket.PlayerEventHandler;
 import xyz.ggos3.lythmServer.socket.RoomEventHandler;
 
 @Component
@@ -12,10 +13,12 @@ public class Runner implements CommandLineRunner {
 
     private final SocketIOServer server;
     private final RoomEventHandler roomEventHandler;
+    private final PlayerEventHandler playerEventHandler;
 
     @Override
     public void run(String... args) throws Exception {
         server.addListeners(roomEventHandler);
+        server.addListeners(playerEventHandler);
         server.start();
     }
 }
